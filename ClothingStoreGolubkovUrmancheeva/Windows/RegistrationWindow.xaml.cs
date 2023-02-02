c8e2f0d082f70991a4801f7ec84b2a512ce0196e
Dmitriy Golubkov <Dima20041504@gmail.com>
2023-02-02 19:52:20 +0300

    1

--- ClothingStoreGolubkovUrmancheeva/Windows/RegistrationWindow.xaml.cs
+++ ClothingStoreGolubkovUrmancheeva/Windows/RegistrationWindow.xaml.cs
@@ -23,5 +23,65 @@ namespace ClothingStoreGolubkovUrmancheeva.Windows
         {
             InitializeComponent();
         }
+
+        private void TbLogin_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TbLogin.Text = "";
+        }
+
+        private void TbLogin_LostFocus(object sender, RoutedEventArgs e)
+        {
+            TbLogin.Text = "Логин";
+        }
+
+        private void TbPassword_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TbPassword.Text = "";
+        }
+
+        private void TbPassword_LostFocus(object sender, RoutedEventArgs e)
+        {
+            TbPassword.Text = "Пароль";
+        }
+
+        private void TbLastName_LostFocus(object sender, RoutedEventArgs e)
+        {
+            TbLastName.Text = "Фамилия";
+        }
+
+        private void TbLastName_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TbLastName.Text = "";
+        }
+
+        private void TbFirstName_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TbFirstName.Text = "";
+        }
+
+        private void TbFirstName_LostFocus(object sender, RoutedEventArgs e)
+        {
+            TbFirstName.Text = "Имя";
+        }
+
+        private void TbPatronymic_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TbPatronymic.Text = "";
+        }
+
+        private void TbPatronymic_LostFocus(object sender, RoutedEventArgs e)
+        {
+            TbPatronymic.Text = "Отчество";
+        }
+
+        private void TbRole_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TbRole.Text = "";
+        }
+
+        private void TbRole_LostFocus(object sender, RoutedEventArgs e)
+        {
+            TbRole.Text = "Должность";
+        }
     }
 }
